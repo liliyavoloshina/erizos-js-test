7c0f0e0780975651aa7e3bca17e866e217e20f63
Li <liliyavoloshine@gmail.com>
2024-06-15 12:54:17 +0500

    feat: 2nd extra

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,68 @@
 import FileManager from "@/components/FileManager"
-import { FileOrFolder } from "@/types/common"
 import { Component } from "react"
 
 import Data from "@/assets/Data.json"
+import { FileOrFolder, isFolder } from "@/types/common"
 
-class App extends Component {
+interface AppState {
+  systemData: FileOrFolder[]
+  searchFilename: string
+}
+
+type AppProps = null
+
+const initialData = JSON.parse(JSON.stringify(Data)) as FileOrFolder[]
+
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props)
+    this.state = {
+      systemData: initialData,
+      searchFilename: "",
+    }
+  }
+
+  filterItems = (value: string, items: FileOrFolder[]) => {
+    return items.filter((item) => {
+      if (isFolder(item)) {
+        const children: FileOrFolder[] = this.filterItems(value, item.children)
+        item.children = children
+        return item.name.includes(value) || children.length
+      } else {
+        return item.name.includes(value)
+      }
+    })
+  }
+
+  onChangeSearch = (value: string) => {
+    this.setState(() => ({ searchFilename: value }))
+
+    const initial = JSON.parse(JSON.stringify(Data)) as FileOrFolder[]
+
+    let newData: FileOrFolder[] = []
+    if (!value) {
+      newData = initial
+    } else {
+      newData = this.filterItems(value, initial)
+    }
+    this.setState(() => ({ systemData: newData }))
+  }
+
   render() {
-    const systemData = Data as FileOrFolder[]
-
     const expandedFolders = ["/Common7", "/Common7/IDE"]
+    const { systemData, searchFilename } = this.state
 
     return (
-      <div className="app">
+      <div className="app container mx-auto p-12">
+        <label className="flex flex-col gap-2 my-12 cursor-pointer">
+          <span className="font-bold">Type filename: </span>
+          <input
+            type="text"
+            value={searchFilename}
+            onChange={(e) => this.onChangeSearch(e.target.value)}
+            className="text-black"
+          />
+        </label>
         <FileManager data={systemData} expandedFolders={expandedFolders} />
       </div>
     )
