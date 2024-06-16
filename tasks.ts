68ed73f93224b9981edf2a8a5bf047e5e22517e9
Li <liliyavoloshine@gmail.com>
2024-06-16 23:34:42 +0500

    feat: 2nd task

--- tasks.ts
+++ tasks.ts
@@ -11,8 +11,62 @@ const customParseInt = (str: string): number => {
     }
 
     for (let i = 0; i < asciiValues.length; i++) {
-        const rightMostDigit = asciiValues.length - i - 1
-        res += asciiValues[i] * 10 ** rightMostDigit;
+        const leftMostDigit = asciiValues.length - i - 1
+        const powerOfDigit = 10 ** leftMostDigit
+        res += asciiValues[i] * powerOfDigit;
+    }
+
+    return res
+}
+
+// 2.
+const spiralMatrix = (matrix: number[][]) => {
+    const res: number[] = []
+
+    let right = matrix[0].length
+    let left = 0
+
+    let bottom = matrix.length
+    let top = 0
+
+    const toTop = (): void => {
+        for (let i = bottom - 1; i >= top; i--) {
+            res.push(matrix[i][left]);
+        }
+        left++;
+    };
+
+    const toBottom = (): void => {
+        for (let i = top; i < bottom; i++) {
+            res.push(matrix[i][right - 1]);
+        }
+        right--;
+    };
+
+    const toRight = (): void => {
+        for (let i = left; i < right; i++) {
+            res.push(matrix[top][i]);
+        }
+        top++;
+    };
+
+    const toLeft = (): void => {
+        for (let i = right - 1; i >= left; i--) {
+            res.push(matrix[bottom - 1][i]);
+        }
+        bottom--;
+    };
+
+
+
+    while (left < right && top < bottom) {
+        toRight()
+        toBottom()
+
+        if (left >= right || top >= bottom) break
+
+        toLeft()
+        toTop()
     }
 
     return res
